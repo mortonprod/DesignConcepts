abf74818b937ff3a8c00342528d9c4024c243c39
Alexander Morton <mortonprod@gmail.com>
2017-10-12 12:17:15 +0100

    Update typings

--- myTypings/index.d.ts
+++ myTypings/index.d.ts
@@ -4,23 +4,3 @@
     calls: number;
 }
 
-interface IState {
-    pos: IVector3d,
-    vel: IVector3d,
-    charge: number;
-    mass: number;
-    radius: number;
-}
-
-interface IVector3d {
-    x: number;
-    y: number;
-    z: number;
-}
-
-interface IDot {
-    start: number;
-    end: number;
-    radius: number;
-    colour: string;
-}
